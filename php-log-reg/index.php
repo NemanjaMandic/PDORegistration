54e529adf64a9f3fa64a0a15d284e38fb4f3bd95
NemanjaMandic <nemanjans84@gmail.com>
2016-09-13 16:57:39 +0200

    dynamic part

--- php-log-reg/index.php
+++ php-log-reg/index.php
@@ -1,1 +1,20 @@
-index.php
+<?php 
+session_start();
+
+if(isset($_SESSION['id'])){
+
+}else{
+	?>
+
+	<a href="index.php">HOME</a> |
+	<a href="index.php?option=register">REGISTRATION</a> |
+	<a href="index.php?option=login">LOGIN</a><hr>
+
+	<?php
+
+	if(isset($_GET['option'])){
+
+	}else{
+		echo "Pocetna stanica";
+	}
+}
