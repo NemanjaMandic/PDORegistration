766592f1d4802361848db1e7cfcda618c67fa585
NemanjaMandic <nemanjans84@gmail.com>
2016-09-19 22:54:11 +0200

    table

--- php-log-reg/profile.php
+++ php-log-reg/profile.php
@@ -2,6 +2,15 @@
   
    if(isset($_GET['pid'])){
 
+   		 $qUsr = "SELECT * FROM `korisnici` 
+   	          WHERE `id` = :user";
+
+   	          $profile = $connector->prepare($qUsr);
+   	          $profile->execute(array(
+
+   	          		':user' => $_GET['pid']
+   	          	));
+
    }else{
    	 $qUsr = "SELECT * FROM `korisnici` 
    	          WHERE `id` = :user";
@@ -12,11 +21,21 @@
    	          		':user' => $_SESSION['id']
    	          	));
 
-   	          $fetchProf = $profile->fetchAll(PDO::FETCH_OBJ);
+   	         
+   }
+
+   if(isset($_SESSION['id'])){
+   		if($profile->rowCount()){
+
+    		 $fetchProf = $profile->fetchAll(PDO::FETCH_OBJ);
    	          foreach($fetchProf as $p){
    	          	echo "<h3>" . $p->name . "</h3>";
    	          	echo $p->username . "<br>";
    	          	echo $p->email . "<br>";
    	          }
-   }
-?>
+   	      }else{
+   	      	echo "No such user";
+   	      }
+   	}else{
+   		echo "You are not authorised to see this page";
+   	}
--- php-log-reg/user_list.php
+++ php-log-reg/user_list.php
@@ -8,8 +8,50 @@ $users = $connector->query($qUser);
 $fUsr = $users->fetchAll(PDO::FETCH_OBJ);
 
 //echo "<pre>", print_r($fUsr), "</pre>";
+?>
 
-foreach ($fUsr as $k) {
-	echo $k->username," ", $k->name," ", $k->email, "<br>";
+<table border="1">
+	<tr>
+		<th>ID</th>
+		<th>Username</th>
+		<th>Name</th>
+		<th>Email</th>
+	</tr>
+
+	<?php
+
+       foreach ($fUsr as $k) {
+	?>
+	<tr>
+	
+      <td>
+	      <a href="index.php?option=profile&pid=
+	          <?php echo $k->id; ?>">
+	      	  <?php echo $k->id; ?>
+	      </a>
+      </td>
+       <td>
+      	<a href="index.php?option=profile&pid=
+	          <?php echo $k->id; ?>">
+	      	  <?php echo $k->username; ?>
+	      </a>
+      </td>
+       <td>
+      	<a href="index.php?option=profile&pid=
+	          <?php echo $k->id; ?>">
+	      	  <?php echo $k->name; ?>
+	      </a>
+      </td>
+       <td>
+      	<a href="index.php?option=profile&pid=
+	          <?php echo $k->id; ?>">
+	      	  <?php echo $k->email; ?>
+	      </a>
+      </td>
+    </tr>
+    <?php
 }
 ?>
+</table>
+
+<?php
