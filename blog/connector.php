d63523005c5328cdc4336a423e5a7435987f1e8b
NemanjaMandic <nemanjans84@gmail.com>
2016-10-01 01:22:05 +0200

    blog connector

--- blog/connector.php
+++ blog/connector.php
@@ -1,1 +1,9 @@
-connector.php
+<?php
+
+try{
+   $connector = new PDO('mysql: host=127.0.0.1; dbname=blog', 'root', '');
+   $connector->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+	echo $e->getMessage();
+	die();
+}
