71c3728dbb5af5b57243a0b536a99eb593f1dc63
NemanjaMandic <nemanjans84@gmail.com>
2016-10-03 17:27:55 +0200

    pre list blogs

--- blog/blogs.php
+++ blog/blogs.php
@@ -1,1 +1,31 @@
-blogs.php
+<?php
+
+echo "<h3>Last Blogs</h3>";
+
+$qBlogs = "
+
+   SELECT `blog`.`id` as `blodz_id`,
+          `blog`.`title` as `blodz_tajtl`,
+          `blog`.`text` as `blodz_text`,
+          `blog`.`date` as `blodz_dejt`,
+          `blog`.`category_id` as `blog_cat_id`,
+          `blog`.`user_id` as `blog_user_id`,
+
+          `blog_category`.`id` as `blog_category_id`,
+          `blog_category`.`name` as `blog_category_name`,
+
+          `users`.`id` as `user_id`,
+          `users`.`username` as `user_username`
+
+          FROM `blog`, `blog_category`, `users`
+
+          WHERE `blog_category`.`id` = `blog`.`category_id`
+          AND `users`.`id` = `blog`.`user_id`
+          GROUP BY `blog`.`id`
+          ORDER BY `blog`.`date` DESC
+";
+
+$blogs = $connector->query($qBlogs);
+$allBlogs = $blogs->fetchAll(PDO::FETCH_OBJ);
+
+echo "<pre>", print_r($allBlogs), "</pre>";
