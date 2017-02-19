96deaffe88af9bcf8b5765e76afb5184f9f47eef
NemanjaMandic <nemanjans84@gmail.com>
2017-02-19 14:18:59 +0100

    get conversations

--- php-log-reg/messages.php
+++ php-log-reg/messages.php
@@ -6,7 +6,43 @@ if(isset($_SESSION['id'])){
 
 	<table style="width: 100%;">
 		<tr>
-			<td style="width: 40%;">Conversations</td>
+			<td style="width: 40%;">
+				<?php 
+                   //mysql za prikaz poruka
+                  $conversations ="
+                  	SELECT 
+                        `razgovori`.`id` as `razgovorId`,
+                        `razgovori`.`naslov` as `razgovorNaslov`,
+
+                        `razgovor_ucesnici`.`korisnik_id` as `razgovorKorisnik`,
+
+                        `poruke`.`vreme` as `porukeVreme`
+                  	FROM
+                         `razgovori`, `razgovor_ucesnici`, `poruke`
+                  	WHERE
+                         `razgovori`.`id` = `razgovor_ucesnici`.`razgovor_id`
+                         AND 
+                         `razgovori`.`id` = `poruke`.`razgovor_id`
+                         AND
+                         `razgovor_ucesnici`.`korisnik_id` = :korisnik
+                  	GROUP BY 
+                  	     `razgovorId`
+                  	ORDER BY
+                  	     `poruke`.`vreme` DESC
+                  ";
+				//kreiranje pdo koda
+                  //priprema za izvrsenje koda
+                  $pRazgovori = $connector->prepare($conversations);
+
+				
+                    $pRazgovori->execute(array(
+                          ':korisnik' => $_SESSION['id']
+                    	));
+				//prikaz podataka
+                $fRazgovori =  $pRazgovori->fetchAll(PDO::FETCH_OBJ);
+                echo "<pre>", print_r($fRazgovori), "</pre>";
+				?>
+			</td>
 			<td style="width: 60%;">Messages in conversation</td>
 		</tr>
 	</table>
